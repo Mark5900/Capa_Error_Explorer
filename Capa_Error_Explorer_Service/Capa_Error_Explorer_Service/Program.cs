ce0e2965a240f535c14b9e6148791eab7fec17bc
Mark B. Rasmussen <mark@boennelykke.com>
2023-11-07 19:38:42 +0100

    Initial commit

---
+++ Capa_Error_Explorer_Service/Capa_Error_Explorer_Service/Program.cs
@@ -0,0 +1,10 @@
+using Capa_Error_Explorer_Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
