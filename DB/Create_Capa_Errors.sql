e0636874ab26aaad793d0990929cd367cc4da207
Mark B. Rasmussen <mark@boennelykke.com>
2023-11-11 12:21:56 +0100

    Update Create_Capa_Errors.sql

--- DB/Create_Capa_Errors.sql
+++ DB/Create_Capa_Errors.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Capa_Errors] (
-    [UmitID]         INT              NOT NULL,
+    [UnitID]         INT              NOT NULL,
     [PackageID]      INT              NOT NULL,
     [Status]         VARCHAR (10)     NULL,
     [LastRunDate]    INT              NULL,
@@ -9,7 +9,7 @@
     [PackageGUID]    UNIQUEIDENTIFIER NOT NULL,
     [UnitName]       VARCHAR (150)    NULL,
     [PackageName]    VARCHAR (50)     NULL,
-    [PackageVersion] VARCHAR (50)     NULL, 
+    [PackageVersion] VARCHAR (50)     NULL,
     [CMPID] INT NULL
 );
 
