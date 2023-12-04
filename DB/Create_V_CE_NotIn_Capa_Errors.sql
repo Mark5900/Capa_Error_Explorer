b264e831c532170527303806c7e7bb2bbad5aa6e
Mark B. Rasmussen <mark@boennelykke.com>
2023-12-04 18:59:54 +0100

    Create Create_V_CE_NotIn_Capa_Errors.sql

---
+++ DB/Create_V_CE_NotIn_Capa_Errors.sql
@@ -0,0 +1,28 @@
+﻿USE [Test]
+GO
+
+/****** Object: View [dbo].[V_CE_NotIn_Capa_Errors] Script Date: 12/4/2023 6:59:36 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE VIEW [dbo].[V_CE_NotIn_Capa_Errors] AS
+SELECT CMS.dbo.UNITJOB.UNITID, CMS.dbo.UNITJOB.JOBID AS PackageID, CMS.dbo.UNITJOB.[STATUS],  CMS.dbo.UNITJOB.LASTRUNDATE, CMS.dbo.UNITJOB.[LOG],
+	CMS.dbo.UNIT.UUID AS UnitUUID, CMS.dbo.UNIT.[NAME] AS UnitName, CMS.dbo.UNIT.[TYPE],
+	CMS.dbo.JOB.[GUID] AS PackageGUID, CMS.dbo.JOB.[NAME] AS PackageName, CMS.dbo.JOB.[VERSION] AS PackageVersion,
+	CMS.dbo.SCHEDULE.RECURRENCE
+FROM CMS.dbo.UNITJOB
+LEFT JOIN Test.dbo.Capa_Errors ON
+	CMS.dbo.UNITJOB.UNITID = Test.dbo.Capa_Errors.UnitID
+		AND CMS.dbo.UNITJOB.JOBID = Test.dbo.Capa_Errors.PackageID
+JOIN CMS.dbo.UNIT ON
+	CMS.dbo.UNITJOB.UNITID = CMS.dbo.UNIT.UNITID
+JOIN CMS.dbo.JOB ON
+	CMS.dbo.UNITJOB.JOBID = CMS.dbo.JOB.JOBID
+JOIN CMS.dbo.SCHEDULE ON
+	CMS.dbo.UNITJOB.JOBID = CMS.dbo.SCHEDULE.ID
+	WHERE Test.dbo.Capa_Errors.UnitID IS NULL
+		AND Test.dbo.Capa_Errors.PackageID IS NULL
