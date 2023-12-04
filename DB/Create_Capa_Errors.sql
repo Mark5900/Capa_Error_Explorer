463ee7ff8f5192401acd5c02a9d22f840c722e3c
Mark B. Rasmussen <mark@boennelykke.com>
2023-12-04 19:00:46 +0100

    Update Create_Capa_Errors.sql

--- DB/Create_Capa_Errors.sql
+++ DB/Create_Capa_Errors.sql
@@ -1,4 +1,14 @@
-﻿CREATE TABLE [dbo].[Capa_Errors] (
+﻿USE [Test]
+GO
+
+/****** Object: Table [dbo].[Capa_Errors] Script Date: 12/4/2023 7:00:28 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Capa_Errors] (
     [UnitID]            INT              NOT NULL,
     [PackageID]         INT              NOT NULL,
     [Status]            VARCHAR (20)     NULL,
@@ -17,4 +27,5 @@
     [CancelledCount]    INT              NULL,
     [PackageRecurrence] NCHAR (10)       NULL
 );
+
 
